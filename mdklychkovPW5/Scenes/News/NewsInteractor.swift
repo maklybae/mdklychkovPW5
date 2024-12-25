1118f22b367764bd2ddf802a490c083458ce6f4a
Maksim Klychkov <maxim.klychkov64@gmail.com>
2024-12-26 00:54:43 +0300

    feat: :sparkles: add use cases to interactor
    loadFreshNews and loadMoreNews

--- mdklychkovPW5/Scenes/News/NewsInteractor.swift
+++ mdklychkovPW5/Scenes/News/NewsInteractor.swift
@@ -8,9 +8,17 @@
 import Foundation
 
 final class NewsInteractor: NewsBuisnessLogic, NewsDataStore {
+    // MARK: - Constants
+    enum Constants {
+        static let rubricId: Int = 4
+        static let pageSize: Int = 8
+    }
+    
     // MARK: - Variables
     private let presenter: NewsPresentaionLogic
     private let worker = NewsWorker()
+    private let decoder: JSONDecoder = JSONDecoder()
+    private var pageIndex: Int = 1
     
     // MARK: - Properties
     var articles: [ArticleModel] = []
@@ -20,11 +28,51 @@ final class NewsInteractor: NewsBuisnessLogic, NewsDataStore {
     }
     
     // MARK: - Use Cases
-    func loadFreshNews(_ request: News.LoadFreshNews.Request) {
-        <#code#>
+    public func loadFreshNews(_ request: News.LoadFreshNews.Request) {
+        worker.fetchNews(rubricId: Constants.rubricId, pageSize: Constants.pageSize, pageIndex: pageIndex) { [weak self] response in
+            DispatchQueue.global(qos: .userInitiated).async {
+                switch response {
+                case .success(let serverResponse):
+                    if let data = serverResponse.data {
+                        let newsPage = try? self?.decoder.decode(NewsPage.self, from: data)
+                        DispatchQueue.main.async {
+                            self?.articles = newsPage?.news ?? []
+                            self?.presenter.presentFreshNews(News.LoadFreshNews.Response())
+                        }
+                    } else {
+                        DispatchQueue.main.async {
+                            print("No data")
+                        }
+                    }
+                case .failure(let error):
+                    DispatchQueue.main.async {
+                        print(error)
+                    }
+                }
+            }
+        }
     }
     
-    func loadMoreNews(_ request: News.LoadMoreNews.Request) {
-        <#code#>
+    public func loadMoreNews(_ request: News.LoadMoreNews.Request) {
+        worker.fetchNews(rubricId: Constants.rubricId, pageSize: Constants.pageSize, pageIndex: pageIndex) { [weak self] response in
+            DispatchQueue.global(qos: .userInitiated).async {
+                switch response {
+                case .success(let serverResponse):
+                    if let data = serverResponse.data {
+                        let newsPage = try? self?.decoder.decode(NewsPage.self, from: data)
+                        DispatchQueue.main.async {
+                            self?.articles.append(contentsOf: newsPage?.news ?? [])
+                            self?.presenter.presentMoreNews(News.LoadMoreNews.Response())
+                        }
+                    }
+                case .failure(let error):
+                    DispatchQueue.main.async {
+                        print(error)
+                    }
+                }
+            }
+        }
     }
+    
+    // MARK: - Private funcs
 }
